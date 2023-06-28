855e9acfa2dbefa9a213c678d6ded8f659f652c2
ithiel883 <ajuka.zephiniah@gmail.com>
2023-06-28 17:59:44 +0100

    added animal array to map to select fields

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"]
 
 
 const SearchParams = () => {
     const [location, setLocation] = useState('')
+    const [animal, setAnimal] = useState('')
+    const [breed, setBreed] = useState('')
     return (
         <div className="search-params">
             {location}
@@ -11,6 +14,17 @@ const SearchParams = () => {
                 <label htmlFor="location">
                    Location
                    <input onChange={e => setLocation(e.target.value)} id="location" value={location} placeholder="Location" /> 
+                </label>
+                <label htmlFor="animal">
+                    Animal
+                    <select id="animal" value={animal} onChange={e => {
+                        setAnimal(e.target.value)
+                    }}>
+                        <option />
+                        {ANIMALS.map((animal) => (
+                            <option key={animal}>{animal}</option>
+                        ))}
+                    </select>
                 </label>
                 <button>Submit</button>
             </form>
